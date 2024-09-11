3ef511c9a6608c8692e288966c9ebfcbd622d5bb
mariaeryka <erykamariab4@gmail.com>
2024-09-11 14:19:17 -0300

    par ou impar

---
+++ att 10.py
@@ -0,0 +1,8 @@
+# Crie um programa que receba um número inteiro e informe se ele é par ou ímpar.
+
+numero = int(input("Digite um número onteiro:"))
+
+if numero%2 == 0:
+    print("Par")
+else:
+    print("ímpar")
